145e89b8cbb88bc0aff82cdae7ec8bcf68813d85
Yagizhan Aydin <yagizhanaydin35@gmail.com>
2024-04-22 18:05:20 +0300

    değişim

--- uyum.js
+++ uyum.js
@@ -1,10 +1,11 @@
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    var password1 = document.getElementById("password1").value;
+    var password2 = document.getElementById("password2").value;
 
-        document.getElementById("passwordForm").addEventListener("submit", function(event) {
-            var password1 = document.getElementById("password1").value;
-            var password2 = document.getElementById("password2").value;
-
-            if (password1 !== password2) {
-                alert("Şifreler uyuşmuyor. Lütfen aynı şifreyi tekrar girin.");
-                event.preventDefault(); // Formun gönderimini engelle
-            }
-        });
+    if (password1 !== password2) {
+        alert("Şifreler eşleşmiyor. Lütfen aynı şifreyi girin.");
+        event.preventDefault(); 
+    } else {
+        alert("Şifreler eşleşti. Artık giriş yapabilirsiniz.");
+    }
+});
