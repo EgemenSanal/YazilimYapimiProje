65a96f10be3f9026cd7d24060484b7f6e03a2f70
Yagizhan Aydin <yagizhanaydin35@gmail.com>
2024-04-23 15:21:55 +0300

    değişiklik

--- uyum.js
+++ uyum.js
@@ -1,13 +1,14 @@
 document.getElementById("passwordForm").addEventListener("submit", function(event) {
-    var password1 = document.getElementById("password1").value;
-    var password2 = document.getElementById("password2").value;
+    var password1 = document.getElementById("password1").value.length;
+    var password2 = document.getElementById("password2").value.length;
 
     if(password1!==password2){
         alert("şfreler uyuşmuyor");
         
     }else{
-        if(password1<6||password1>20){
+        if(password1.length<6&&password1.length>20){
             alert("şifre 6 ile 20 karakter arasında olmalıdır");
         }
+        
     }
 });
