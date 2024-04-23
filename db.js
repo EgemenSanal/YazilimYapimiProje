ae7a13cecf0c8d8b2e9a931794a320016e8001b2
Egemen Sanal <egemensanal@gmail.com>
2024-04-23 19:39:09 +0300

    kayit olma database

---
+++ db.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyB1T5KUM_bnOcJIV6XB8Ml6zXYxqKzDYHE",
+
+  authDomain: "fir-login-63cd9.firebaseapp.com",
+
+  databaseURL: "https://fir-login-63cd9-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "fir-login-63cd9",
+
+  storageBucket: "fir-login-63cd9.appspot.com",
+
+  messagingSenderId: "73596038394",
+
+  appId: "1:73596038394:web:474094adb57d07990bed43"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener("click",function(event){
+    
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('exampleInputEmail1').value;
+    const password = document.getElementById('exampleInputPassword1').value;
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  event.preventDefault();
+})
+
+
+
