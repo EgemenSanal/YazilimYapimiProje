4a87cafb1a9334c58cef218b9ff4e0a622b2d0ad
Egemen Sanal <egemensanal@gmail.com>
2024-04-24 15:17:00 +0300

    sifre reset maili eklendi

---
+++ forgotPassword.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+
+import { getAuth,sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyB1T5KUM_bnOcJIV6XB8Ml6zXYxqKzDYHE",
+
+  authDomain: "fir-login-63cd9.firebaseapp.com",
+
+  databaseURL: "https://fir-login-63cd9-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "fir-login-63cd9",
+
+  storageBucket: "fir-login-63cd9.appspot.com",
+
+  messagingSenderId: "73596038394",
+
+  appId: "1:73596038394:web:474094adb57d07990bed43"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const forgotPasswordButton = document.getElementById('forgotPasswordLabel');
+
+
+forgotPasswordLabel = document.getElementById('forgotPasswordLabel');
+
+forgotPasswordLabel.addEventListener('click',function(event){
+    const email = document.getElementById('gmail').value;
+    sendPasswordResetEmail(auth,email)
+  .then(() =>{
+    alert("SIFRE YENILEME MAILI E POSTANIZA GONDERILDI!")
+  })
+  .catch((error) =>{
+    console.log(error.code);
+    console.log(error.message);
+
+  })
+});
