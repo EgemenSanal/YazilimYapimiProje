caf0976d1b844622f37577eb03a6e8dc19cdd754
Egemen Sanal <egemensanal@gmail.com>
2024-04-27 21:25:06 +0300

    gereksiz js dosyasi silindi

--- görme.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("passwordForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    var passwordField = document.getElementById("password");
-    var password = passwordField.value;
-    alert("Girilen Şifre: " + password);
-});
